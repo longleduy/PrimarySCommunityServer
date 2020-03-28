259f989660e89e7f6a6b37bf101e24e7a093ba7f
LongLD <longld@fabbi.io>
2020-03-29 01:12:42 +0700

    create branch dv-1

---
+++ README.md
@@ -0,0 +1,7 @@
+- Connect MongoDB Atlas
+	mongodb+srv://<username>:<password>@cluster0-bgjen.mongodb.net/test
+	supper_user_1
+	app_owner_01
+	app_read_01
+- Redis Lab
+	redis-17888.c1.ap-southeast-1-1.ec2.cloud.redislabs.com:17888 longkhanh@@
--- src/configs/redisDBConnect.js
+++ src/configs/redisDBConnect.js
@@ -9,7 +9,15 @@ Bluebird.promisifyAll(redis.RedisClient.prototype)
 Bluebird.promisifyAll(redis.Multi.prototype)
 
 const RedisStore = redisConnect(session);
-export const client = redis.createClient()
+export const client = redis.createClient({
+    port : process.env.REDIS_PORT,
+    host : process.env.REDIS_HOST
+})
+client.auth(process.env.REDIS_AUTH, function(err, response){
+    if(err){
+    throw err;
+    }
+    });
 client.on('connect', function () {
     if (process.env.NODE_ENV != 'test') {
         console.log(`RedisDB connecting...`)
@@ -22,7 +30,7 @@ export const redisServer = session({
     secret: process.env.REDIS_SECRET_SESSION,
     resave: false,
     saveUninitialized: false,
-    store: new RedisStore({ host: 'localhost', port: 6379, client: client }),
+    store: new RedisStore({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, client: client }),
     cookie: {}
 });
 export const asyncClient = Promise.promisifyAll(client);
